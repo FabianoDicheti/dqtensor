ee8a47d1e22231677072fef043dfd756c15d1676
Fabiano Dicheti <fabiano.dicheti@gmail.com>
2025-03-13 11:57:24 -0300

    add nadam

--- src/optimizers/bp_optimizers.rs
+++ src/optimizers/bp_optimizers.rs
@@ -24,6 +24,7 @@ pub struct Adam {
     rmsprop_second: Vec<f64>,
 }
 
+
 impl Adam {
     /// Cria uma nova instância do otimizador Adam.
     /// # Argumentos
@@ -84,6 +85,72 @@ impl Optimizer for Adam {
             let v_hat = self.rmsprop_second[i] / (1.0 - beta2_t);
 
             // Atualiza parâmetros
+            params[i] -= self.learning_rate * m_hat / (v_hat.sqrt() + self.epsilon);
+        }
+    }
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/// Implementação do Nadam (Nesterov-accelerated Adam)
+/// Incorpora a aceleração de Nesterov na estimativa do primeiro momento
+pub struct Nadam {
+    learning_rate: f64,
+    beta1: f64,
+    beta2: f64,
+    epsilon: f64,
+    time_step: u64,
+    momentum_first: Vec<f64>,  // Primeiro momento (m)
+    rmsprop_second: Vec<f64>, // Segundo momento (v)
+}
+
+impl Nadam {
+    pub fn new(
+        learning_rate: f64,
+        beta1: f64,
+        beta2: f64,
+        epsilon: f64,
+        param_size: usize,
+    ) -> Self {
+        Self {
+            learning_rate,
+            beta1,
+            beta2,
+            epsilon,
+            time_step: 0,
+            momentum_first: vec![0.0; param_size],
+            rmsprop_second: vec![0.0; param_size],
+        }
+    }
+}
+
+impl Optimizer for Nadam {
+    fn update(&mut self, params: &mut [f64], grads: &[f64]) {
+        debug_assert_eq!(params.len(), grads.len());
+        debug_assert_eq!(params.len(), self.momentum_first.len());
+        debug_assert_eq!(params.len(), self.rmsprop_second.len());
+
+        self.time_step += 1;
+        let t = self.time_step;
+
+        // Cálculo das potências beta para correção de viés
+        let beta1_pow_t = self.beta1.powi(t as i32);
+        let beta1_pow_t_plus_1 = self.beta1 * beta1_pow_t; // β1^(t+1)
+        let beta2_pow_t = self.beta2.powi(t as i32);
+
+        for i in 0..params.len() {
+            // Atualiza os momentos (igual ao Adam)
+            self.momentum_first[i] = self.beta1 * self.momentum_first[i] + (1.0 - self.beta1) * grads[i];
+            self.rmsprop_second[i] = self.beta2 * self.rmsprop_second[i] + (1.0 - self.beta2) * grads[i].powi(2);
+
+            // Ajuste de Nesterov: combinação do momento atual e gradiente futuro
+            let m_hat = (self.beta1 * self.momentum_first[i]) / (1.0 - beta1_pow_t_plus_1)
+                + ((1.0 - self.beta1) * grads[i]) / (1.0 - beta1_pow_t);
+
+            // Correção de viés do segundo momento (igual ao Adam)
+            let v_hat = self.rmsprop_second[i] / (1.0 - beta2_pow_t);
+
+            // Atualização dos parâmetros
             params[i] -= self.learning_rate * m_hat / (v_hat.sqrt() + self.epsilon);
         }
     }
