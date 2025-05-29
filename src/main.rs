6c3f1095a09ae9bde1481256c3f26b34abcb5bdf
Fabiano Dicheti <fabiano.dicheti@gmail.com>
2025-05-29 00:14:32 -0300

    update main

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
-// mod lstm_test;
-// mod mlp_test;
+mod lstm_test;
+mod mlp_test;
 mod mlp_test2;
 
 fn main() {
 
 
-    // lstm_test::main();
+    ;
 
-    //  if let Err(e) = mlp_test::run_experiment() {
-    //      eprintln!("Erro ao executar experimento Iris: {}", e);
-    //  }
+     if let Err(e) = mlp_test::run_experiment() {
+         eprintln!("Experiment MLP Iris: {}", e);
+     }
+
      if let Err(e) = mlp_test2::run_experiment_wine() {
-         eprintln!("Erro ao executar experimento Wine: {}", e);
+         eprintln!("Experiment MLP Wine: {}", e);
+     }
+
+      if let Err(e) = lstm_test::main() {
+         eprintln!("Experiment LSTM Jena Climate: {}", e);
      }
  }
