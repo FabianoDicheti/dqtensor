4a03bb0c4db2b14ab2fbc497014afa063de588bd
fabiano dicheti <fabianodicheti@MacBook-Air-de-fabiano.local>
2025-07-28 13:55:37 -0300

    teste main

--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,15 @@ fn main() {
          eprintln!("mamba test: {}", e);
      }
 
-     if let Err(e) = mlp_test::run_experiment() {
-         eprintln!("Experiment MLP Iris: {}", e);
-     }
+    //  if let Err(e) = mlp_test::run_experiment() {
+    //      eprintln!("Experiment MLP Iris: {}", e);
+    //  }
 
-     if let Err(e) = mlp_test2::run_experiment_wine() {
-         eprintln!("Experiment MLP Wine: {}", e);
-     }
+    //  if let Err(e) = mlp_test2::run_experiment_wine() {
+    //      eprintln!("Experiment MLP Wine: {}", e);
+    //  }
 
-      if let Err(e) = lstm_test::main() {
-         eprintln!("Experiment LSTM Jena Climate: {}", e);
-     }
+    //   if let Err(e) = lstm_test::main() {
+    //      eprintln!("Experiment LSTM Jena Climate: {}", e);
+    //  }
  }
